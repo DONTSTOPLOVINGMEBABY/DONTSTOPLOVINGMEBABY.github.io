7cd4f9484539b58ae14871758cfbb2ba5604fb4c
Henry Jacobs <henryjacobscode@gmail.com>
2023-09-01 13:42:23 -0400

    Get about section about where I want it

--- src/components/about-section/about.jsx
+++ src/components/about-section/about.jsx
@@ -4,7 +4,8 @@ import {
     WrapPhoto, 
     AboutInfo, 
     Bio, 
-    ScrollerContainer, 
+    ScrollerContainer,
+    DescribeMyself,  
 } from "../../styles/about.styled"
 import SectionSplitter from "../section-splitter"
 import ScrollingText from "../scroll-text"
@@ -17,29 +18,30 @@ function AboutSection () {
                 <WrapPhoto>
                     <PhotoOfMe src="https://hjacobs-rest-api-production.up.railway.app/henryjacobs/me" alt="me!"/>
                 </WrapPhoto>
-                <Bio>
-                    My name is Henry Jacobs and I am a Full Stack Developer who is incredibly passionate about computers and the positive impacts they can bring to people's lives! I first started programming at University in 2019 and have been exploring the ins and outs of web development since 2022 with my work on The Odin Project. I'm totally enthralled by computers and learning as much as I possibly can about them from the physical representation of bits to arranging pixels on the screen with HTML and CSS!
-                </Bio>
-                <ScrollerContainer>
-
-                    <h3>My Interests Are</h3>
-                    <ScrollingText
-                    text="Computing &middot;"
-                    direction="toRight"
-                    />
-                    <ScrollingText
-                    text="Economics &middot;"
-                    direction="toLeft"
-                    />
-                    <ScrollingText
-                    text="Math &middot;"
-                    direction="toLeft"
-                    />
-                    <ScrollingText
-                    text="Physics &middot;"
-                    direction="toRight"
-                    />
-                </ScrollerContainer>
+                <DescribeMyself>
+                    <Bio>
+                        My name is Henry Jacobs and I am a Full Stack Developer who is incredibly passionate about computers and the positive impacts they can bring to people's lives! I first started programming at University in 2019 and have been exploring the ins and outs of web development since 2022 with my work on The Odin Project. I'm totally enthralled by computers and learning as much as I possibly can about them from the physical representation of bits to arranging pixels on the screen with HTML and CSS!
+                    </Bio>
+                    <ScrollerContainer>
+                        <h3>My Interests Are</h3>
+                        <ScrollingText
+                        text="Computer Science &middot;"
+                        direction="toLeft"
+                        />
+                        <ScrollingText
+                        text="Economics &middot;"
+                        direction="toRight"
+                        />
+                        <ScrollingText
+                        text="Mathematics &middot;"
+                        direction="toLeft"
+                        />
+                        <ScrollingText
+                        text="Physics &middot;"
+                        direction="toRight"
+                        />
+                    </ScrollerContainer>
+                </DescribeMyself>
             </AboutInfo>
         </AboutPageStyled>
     )
--- src/styles/about.styled.jsx
+++ src/styles/about.styled.jsx
@@ -1,28 +1,89 @@
 import styled from "styled-components"
 
 
-const AboutPageStyled = styled.div`
+const AboutPageStyled = styled.section`
     width: 100%;
-    height: 500px;
+    height: fit-content ; 
     padding: 20px;
     margin-top: 30px;
     display: flex;
     flex-direction: column ; 
+    margin-bottom: 400px;
 `
 
 const AboutInfo = styled.div`
+    padding: 20px 0;
     width: 100%;
     flex: 1;
     display: flex;
+    @media (max-width: 850px) {
+        flex-direction: column;
+    }
+`
+
+const WrapPhoto = styled.div`
+    flex: 1;
+    width: fit-content;
+    margin-right: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    @media (max-width: 850px) {
+        width: 100%;
+        justify-content: center;
+    }
 `
 
 const PhotoOfMe = styled.img`
-    height: 300px;
-    border-radius: 50%;
+    height: 100%;
+    max-height: 500px;
+    border-radius: 20px;
+    object-fit: contain;
+    @media (max-width: 850px) {
+        margin-bottom: 20px;
+    }
 `
+
+const DescribeMyself = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+`
+
+const Bio = styled.article`
+    font-size: 1;
+    line-height: 2rem;
+    flex: 1;
+    text-align: left;
+    color: #999999;
+    @media (max-width: 850px) {
+        text-align: center;
+    }
+`
+
+const ScrollerContainer = styled.div`
+    margin: 30px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    h3 {
+        font-family: 'Big Shoulders Stencil Display', cursive;
+        font-size: 24px;
+        font-weight: 400;
+        text-align: center;
+    }
+
+`
+
 
 export {
     AboutPageStyled,
+    WrapPhoto, 
     PhotoOfMe,  
     AboutInfo, 
+    Bio, 
+    ScrollerContainer, 
+    DescribeMyself, 
 }
