2c0a3b49568809e46b012f98971ea181e570d672
DONTSTOPLOVINGMEBABY <henryjacobscode@gmail.com>
2024-03-18 11:32:25 -0700

    wrap resume link in feature flag

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,33 +1,36 @@
-import { 
-    HeaderStyled, 
-    Name, 
-    NavBar, 
-    NavItem, 
-    NavLink, 
+import {
+    HeaderStyled,
+    Name,
+    NavBar,
+    NavItem,
+    NavLink,
 } from "../styles/header.styled"
 import useWindowSize from '../hooks/useWindowSize'
+import { useFlag } from 'feature-toggles-react-sdk'
 
-function Header () {
+function Header() {
 
     const screenSize = useWindowSize()
 
-    return ( 
+    const showResumeLink = useFlag('Page-Header.resume-link')
+
+    return (
         screenSize.width > 700 ? <HeaderStyled>
             <Name>Henry Jacobs</Name>
             <NavBar>
                 <NavItem href="#about">ABOUT</NavItem>
                 <NavItem href="#projects">PROJECTS</NavItem>
                 <NavItem href="#contact">CONTACT</NavItem>
-                {/* <NavItem href="https://hjacobs-rest-api-production.up.railway.app/henryjacobs/resume" target="_blank">RESUME</NavItem> */}
-            </NavBar>
-        </HeaderStyled> 
-        : <HeaderStyled> 
-            <Name>Henry Jacobs</Name>
-            <NavBar>
-                <NavItem href="https://hjacobs-rest-api-production.up.railway.app/henryjacobs/resume" target="_blank">RESUME</NavItem>
+                {showResumeLink ? <NavItem href="https://hjacobs-rest-api-production.up.railway.app/henryjacobs/resume" target="_blank">RESUME</NavItem> : null}
             </NavBar>
         </HeaderStyled>
-    )  
+            : <HeaderStyled>
+                <Name>Henry Jacobs</Name>
+                <NavBar>
+                    {showResumeLink ? <NavItem href="https://hjacobs-rest-api-production.up.railway.app/henryjacobs/resume" target="_blank">RESUME</NavItem> : null}
+                </NavBar>
+            </HeaderStyled>
+    )
 }
 
 export default Header
